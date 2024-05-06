8f8e1ad5305aff181787985966db944ff1dd3086
blebeuf <bryanlebeuf@gmail.com>
2024-05-06 14:19:32 -0500

    reiterate and update models

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,16 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true // Enable auto-increment for the 'id' field
+    },
+    tag_name: {
+      type: DataTypes.STRING, // Set the data type of the 'tag_name' field to string
+      // ALSO one could do allowNull defaults to true, allowing 'tag_name' to be optional
+    }
   },
   {
     sequelize,
