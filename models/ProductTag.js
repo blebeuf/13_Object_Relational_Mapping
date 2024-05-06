a702b19ef69928998b2652334373ac27b9d593ac
blebeuf <bryanlebeuf@gmail.com>
2024-05-06 14:12:34 -0500

    model updates

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,27 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', // Specifies the table name this foreign key references, which is 'product'.
+      key: 'id', // The specific column in the 'product' table that 'product_id' refers to.
+      unique: false // Indicates that multiple entries can share the same 'product_id' (not a unique constraint). 
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag', // Specifies the table name this foreign key references, which is 'tag'.
+        key: 'id', // The specific column in the 'tag' table that 'tag_id' refers to.
+        unique: false // Indicates that multiple entries can share the same 'tag_id' (not a unique constraint).
+      }
+    }
   },
   {
     sequelize,
