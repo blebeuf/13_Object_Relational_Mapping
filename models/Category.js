fdfc62651915e93bdc34b8a6eac2410b54a394b3
blebeuf <bryanlebeuf@gmail.com>
2024-05-06 13:48:21 -0500

    updating the models_category

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,24 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+
+// Creating a new class 'Category' that extends Sequelize's Model class.
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+       // Define the 'id' field with specific properties.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // Define the 'category_name' field with specific properties.
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
